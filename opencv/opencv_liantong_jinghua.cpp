f6d5707d31534f01ffc879f6f68ea14a802b391d
tsingjinyun <dongyxin@126.com>
2013-11-30 13:17:56 +0800

    first commit

---
+++ opencv/opencv_liantong_jinghua.cpp
@@ -0,0 +1,486 @@
+﻿/**
+	比平均背景法性能更加良好的方法,codeBook模型实现背景减除
+	
+	此代码加入了用连通成分分析来清理原始分割图像的方法
+
+	核心代码详细解析和实现 by zcube
+*/
+
+/************************************************************************/
+/*			A few more thoughts on codebook models
+In general, the codebook method works quite well across a wide number of conditions, 
+and it is relatively quick to train and to run. It doesn’t deal well with varying patterns of 
+light — such as morning, noon, and evening sunshine — or with someone turning lights 
+on or off indoors. This type of global variability can be taken into account by using 
+several different codebook models, one for each condition, and then allowing the condition 
+to control which model is active.		                                */
+/************************************************************************/
+
+#include "stdafx.h"
+#include <cv.h>
+#include <cxcore.h>		
+#include "D:\openCv2.3.1\1.0\OpenCV\otherlibs\highgui\highgui.h"
+
+int CVCONTOUR_APPROX_LEVEL = 2;   // Approx.threshold - the bigger it is, the simpler is the boundary
+int CVCLOSE_ITR = 1;				// How many iterations of erosion and/or dialation there should be
+
+//For learning background
+
+//Just some convenience macros
+#define CV_CVX_WHITE	CV_RGB(0xff,0xff,0xff)
+#define CV_CVX_BLACK	CV_RGB(0x00,0x00,0x00)
+
+
+#define CHANNELS 3		
+// 设置处理的图像通道数,要求小于等于图像本身的通道数
+
+///////////////////////////////////////////////////////////////////////////
+// 下面为码本码元的数据结构
+// 处理图像时每个像素对应一个码本,每个码本中可有若干个码元
+// 当涉及一个新领域,通常会遇到一些奇怪的名词,不要被这些名词吓坏,其实思路都是简单的
+typedef struct ce {
+	uchar	learnHigh[CHANNELS];	// High side threshold for learning
+	// 此码元各通道的阀值上限(学习界限)
+	uchar	learnLow[CHANNELS];		// Low side threshold for learning
+	// 此码元各通道的阀值下限
+	// 学习过程中如果一个新像素各通道值x[i],均有 learnLow[i]<=x[i]<=learnHigh[i],则该像素可合并于此码元
+	uchar	max[CHANNELS];			// High side of box boundary
+	// 属于此码元的像素中各通道的最大值
+	uchar	min[CHANNELS];			// Low side of box boundary
+	// 属于此码元的像素中各通道的最小值
+	int		t_last_update;			// This is book keeping to allow us to kill stale entries
+	// 此码元最后一次更新的时间,每一帧为一个单位时间,用于计算stale
+	int		stale;					// max negative run (biggest period of inactivity)
+	// 此码元最长不更新时间,用于删除规定时间不更新的码元,精简码本
+} code_element;						// 码元的数据结构
+
+typedef struct code_book {
+	code_element	**cb;
+	// 码元的二维指针,理解为指向码元指针数组的指针,使得添加码元时不需要来回复制码元,只需要简单的指针赋值即可
+	int				numEntries;
+	// 此码本中码元的数目
+	int				t;				// count every access
+	// 此码本现在的时间,一帧为一个时间单位
+} codeBook;							// 码本的数据结构
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// int updateCodeBook(uchar *p, codeBook &c, unsigned cbBounds)
+// Updates the codebook entry with a new data point
+//
+// p			Pointer to a YUV pixel
+// c			Codebook for this pixel
+// cbBounds		Learning bounds for codebook (Rule of thumb: 10)
+// numChannels	Number of color channels we're learning
+//
+// NOTES:
+//		cvBounds must be of size cvBounds[numChannels]
+//
+// RETURN
+//	codebook index
+int cvupdateCodeBook(uchar *p, codeBook &c, unsigned *cbBounds, int numChannels)
+{
+	if(c.numEntries == 0) c.t = 0;
+	// 码本中码元为零时初始化时间为0
+	c.t += 1;	// Record learning event
+	// 每调用一次加一,即每一帧图像加一
+	
+	//SET HIGH AND LOW BOUNDS
+	int n;
+	unsigned int high[3],low[3];
+	for (n=0; n<numChannels; n++)
+	{
+		high[n] = *(p+n) + *(cbBounds+n);
+		// *(p+n) 和 p[n] 结果等价,经试验*(p+n) 速度更快
+		if(high[n] > 255) high[n] = 255;
+		low[n] = *(p+n)-*(cbBounds+n);
+		if(low[n] < 0) low[n] = 0;
+		// 用p 所指像素通道数据,加减cbBonds中数值,作为此像素阀值的上下限
+	}
+
+	//SEE IF THIS FITS AN EXISTING CODEWORD
+	int matchChannel;	
+	int i;
+	for (i=0; i<c.numEntries; i++)
+	{
+		// 遍历此码本每个码元,测试p像素是否满足其中之一
+		matchChannel = 0;
+		for (n=0; n<numChannels; n++)
+			//遍历每个通道
+		{
+			if((c.cb[i]->learnLow[n] <= *(p+n)) && (*(p+n) <= c.cb[i]->learnHigh[n])) //Found an entry for this channel
+			// 如果p 像素通道数据在该码元阀值上下限之间
+			{	
+				matchChannel++;
+			}
+		}
+		if (matchChannel == numChannels)		// If an entry was found over all channels
+			// 如果p 像素各通道都满足上面条件
+		{
+			c.cb[i]->t_last_update = c.t;
+			// 更新该码元时间为当前时间
+			// adjust this codeword for the first channel
+			for (n=0; n<numChannels; n++)
+				//调整该码元各通道最大最小值
+			{
+				if (c.cb[i]->max[n] < *(p+n))
+					c.cb[i]->max[n] = *(p+n);
+				else if (c.cb[i]->min[n] > *(p+n))
+					c.cb[i]->min[n] = *(p+n);
+			}
+			break;
+		}
+	}
+
+	// ENTER A NEW CODE WORD IF NEEDED
+	if(i == c.numEntries)  // No existing code word found, make a new one
+	// p 像素不满足此码本中任何一个码元,下面创建一个新码元
+	{
+		code_element **foo = new code_element* [c.numEntries+1];
+		// 申请c.numEntries+1 个指向码元的指针
+		for(int ii=0; ii<c.numEntries; ii++)
+			// 将前c.numEntries 个指针指向已存在的每个码元
+			foo[ii] = c.cb[ii];
+		
+		foo[c.numEntries] = new code_element;
+		// 申请一个新的码元
+		if(c.numEntries) delete [] c.cb;
+		// 删除c.cb 指针数组
+		c.cb = foo;
+		// 把foo 头指针赋给c.cb
+		for(n=0; n<numChannels; n++)
+			// 更新新码元各通道数据
+		{
+			c.cb[c.numEntries]->learnHigh[n] = high[n];
+			c.cb[c.numEntries]->learnLow[n] = low[n];
+			c.cb[c.numEntries]->max[n] = *(p+n);
+			c.cb[c.numEntries]->min[n] = *(p+n);
+		}
+		c.cb[c.numEntries]->t_last_update = c.t;
+		c.cb[c.numEntries]->stale = 0;
+		c.numEntries += 1;
+	}
+
+	// OVERHEAD TO TRACK POTENTIAL STALE ENTRIES
+	for(int s=0; s<c.numEntries; s++)
+	{
+		// This garbage is to track which codebook entries are going stale
+		int negRun = c.t - c.cb[s]->t_last_update;
+		// 计算该码元的不更新时间
+		if(c.cb[s]->stale < negRun) 
+			c.cb[s]->stale = negRun;
+	}
+
+	// SLOWLY ADJUST LEARNING BOUNDS
+	for(n=0; n<numChannels; n++)
+		// 如果像素通道数据在高低阀值范围内,但在码元阀值之外,则缓慢调整此码元学习界限
+	{
+		if(c.cb[i]->learnHigh[n] < high[n]) 
+			c.cb[i]->learnHigh[n] += 1;
+		if(c.cb[i]->learnLow[n] > low[n]) 
+			c.cb[i]->learnLow[n] -= 1;
+	}
+
+	return(i);
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+// uchar cvbackgroundDiff(uchar *p, codeBook &c, int minMod, int maxMod)
+// Given a pixel and a code book, determine if the pixel is covered by the codebook
+//
+// p		pixel pointer (YUV interleaved)
+// c		codebook reference
+// numChannels  Number of channels we are testing
+// maxMod	Add this (possibly negative) number onto max level when code_element determining if new pixel is foreground
+// minMod	Subract this (possible negative) number from min level code_element when determining if pixel is foreground
+//
+// NOTES:
+// minMod and maxMod must have length numChannels, e.g. 3 channels => minMod[3], maxMod[3].
+//
+// Return
+// 0 => background, 255 => foreground
+uchar cvbackgroundDiff(uchar *p, codeBook &c, int numChannels, int *minMod, int *maxMod)
+{
+	// 下面步骤和背景学习中查找码元如出一辙
+	int matchChannel;
+	//SEE IF THIS FITS AN EXISTING CODEWORD
+	int i;
+	for (i=0; i<c.numEntries; i++)
+	{
+		matchChannel = 0;
+		for (int n=0; n<numChannels; n++)
+		{
+			if ((c.cb[i]->min[n] - minMod[n] <= *(p+n)) && (*(p+n) <= c.cb[i]->max[n] + maxMod[n]))
+				matchChannel++; //Found an entry for this channel
+			else
+				break;
+		}
+		if (matchChannel == numChannels)
+			break; //Found an entry that matched all channels
+	}
+	if(i == c.numEntries) 
+		// p像素各通道值满足码本中其中一个码元,则返回白色
+		return(255);
+
+	return(0);
+}
+
+
+//UTILITES/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//int clearStaleEntries(codeBook &c)
+// After you've learned for some period of time, periodically call this to clear out stale codebook entries
+//
+//c		Codebook to clean up
+//
+// Return
+// number of entries cleared
+int cvclearStaleEntries(codeBook &c)
+{
+	int staleThresh = c.t >> 1;			// 设定刷新时间
+	int *keep = new int [c.numEntries];	// 申请一个标记数组
+	int keepCnt = 0;					// 记录不删除码元数目
+	//SEE WHICH CODEBOOK ENTRIES ARE TOO STALE
+	for (int i=0; i<c.numEntries; i++)
+		// 遍历码本中每个码元
+	{
+		if (c.cb[i]->stale > staleThresh)	
+			// 如码元中的不更新时间大于设定的刷新时间,则标记为删除
+			keep[i] = 0; //Mark for destruction
+		else
+		{
+			keep[i] = 1; //Mark to keep
+			keepCnt += 1;
+		}
+	}
+
+	// KEEP ONLY THE GOOD
+	c.t = 0;						//Full reset on stale tracking
+	// 码本时间清零
+	code_element **foo = new code_element* [keepCnt];
+	// 申请大小为keepCnt 的码元指针数组
+	int k=0;
+	for(int ii=0; ii<c.numEntries; ii++)
+	{
+		if(keep[ii])
+		{
+			foo[k] = c.cb[ii];
+			foo[k]->stale = 0;		//We have to refresh these entries for next clearStale
+			foo[k]->t_last_update = 0;
+			k++;
+		}
+	}
+	//CLEAN UP
+	delete [] keep;
+	delete [] c.cb;
+	c.cb = foo;
+	// 把foo 头指针地址赋给c.cb 
+	int numCleared = c.numEntries - keepCnt;
+	// 被清理的码元个数
+	c.numEntries = keepCnt;
+	// 剩余的码元地址
+	return(numCleared);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//void cvconnectedComponents(IplImage *mask, int poly1_hull0, float perimScale, int *num, CvRect *bbs, CvPoint *centers)
+// This cleans up the foreground segmentation mask derived from calls to cvbackgroundDiff
+//
+// mask			Is a grayscale (8 bit depth) "raw" mask image which will be cleaned up
+//
+// OPTIONAL PARAMETERS:
+// poly1_hull0	If set, approximate connected component by (DEFAULT) polygon, or else convex hull (0)
+// perimScale 	Len = image (width+height)/perimScale.  If contour len < this, delete that contour (DEFAULT: 4)
+
+void cvconnectedComponents(IplImage *mask, int poly1_hull0, float perimScale)
+{
+	static CvMemStorage*	mem_storage	= NULL;
+	static CvSeq*			contours	= NULL;
+	//CLEAN UP RAW MASK
+	cvMorphologyEx( mask, mask, NULL, NULL, CV_MOP_OPEN, CVCLOSE_ITR );
+	// 开运算先腐蚀在膨胀,腐蚀可以清除噪点,膨胀可以修复裂缝
+	cvMorphologyEx( mask, mask, NULL, NULL, CV_MOP_CLOSE, CVCLOSE_ITR );
+	// 闭运算先膨胀后腐蚀,之所以在开运算之后,因为噪点膨胀后再腐蚀,是不可能去除的
+
+	//FIND CONTOURS AROUND ONLY BIGGER REGIONS
+	if( mem_storage == NULL ) 
+		mem_storage = cvCreateMemStorage(0);
+	else cvClearMemStorage(mem_storage);
+
+	CvContourScanner scanner = cvStartFindContours(mask, mem_storage,
+		sizeof(CvContour), CV_RETR_EXTERNAL);
+	// 在之前已讨论过这种轮廓方法,如果只提取轮廓没有cvFindContours()来的方便
+	// 但下面要对轮廓进行直接操作,看来这种方法功能更强大一点
+	
+	//丢弃太小的轮廓
+	CvSeq* c;
+	int numCont = 0;
+	while((c = cvFindNextContour(scanner)) != NULL)
+	{
+		double len = cvContourPerimeter(c);
+		// 计算轮廓周长
+		double q = (mask->height + mask->width)/perimScale;   
+		// calculate perimeter len threshold
+		if( len < q ) 
+			// Get rid of blob if it's perimeter is too small
+			cvSubstituteContour(scanner, NULL);
+		else 
+			// Smooth it's edges if it's large enough
+		{
+			CvSeq* c_new;
+			if(poly1_hull0) 
+				// Polygonal approximation of the segmentation
+				c_new = cvApproxPoly(c, sizeof(CvContour), mem_storage,
+							CV_POLY_APPROX_DP, CVCONTOUR_APPROX_LEVEL);
+			else 
+				// Convex Hull of the segmentation
+				c_new = cvConvexHull2(c,mem_storage, CV_CLOCKWISE, 1);
+			
+			cvSubstituteContour(scanner, c_new);
+			// 替换轮廓扫描器中提取的轮廓
+			numCont++;
+			// 轮廓数目
+		}
+	}
+
+	contours = cvEndFindContours(&scanner);
+	// 结束扫描过程，并且返回最高层的第一个轮廓的指针
+
+	// PAINT THE FOUND REGIONS BACK INTO THE IMAGE
+	cvZero( mask );
+	// 将掩模图像清零
+	// 掩模: 指是由0和1组成的一个二进制图像
+
+	
+	//和书上相比少花了外界矩形和区域中心
+	//JUST DRAW PROCESSED CONTOURS INTO THE MASK
+		for( c=contours; c != NULL; c = c->h_next )
+		cvDrawContours(mask,c,CV_CVX_WHITE, CV_CVX_BLACK,-1,CV_FILLED,8);
+}
+
+int main()
+{
+	///////////////////////////////////////
+	// 需要使用的变量
+	CvCapture*	capture = NULL;
+	IplImage*	rawImage = NULL;
+	IplImage*	yuvImage = NULL;
+	IplImage*	ImaskCodeBook = NULL;
+
+	IplImage*	ImaskCodeBookCC = NULL;
+	
+	codeBook*	cB = NULL;
+	unsigned	cbBounds[CHANNELS];
+	uchar*		pColor = NULL; //YUV pointer
+	int			imageLen = 0;
+	int			nChannels = CHANNELS;
+	int			minMod[CHANNELS];
+	int			maxMod[CHANNELS];
+	
+	//////////////////////////////////////////////////////////////////////////
+	// 初始化各变量
+	cvNamedWindow("Raw");
+	cvNamedWindow("CodeBook");
+	cvNamedWindow("CodeBook_ConnectComp");		
+
+	capture = cvCreateFileCapture("D:\\vs程序\\vs2010\\第9章\\616.mp4");
+	if (!capture)
+	{
+		printf("Couldn't open the capture!");
+		return -1;
+	}
+
+	rawImage = cvQueryFrame(capture);
+	yuvImage = cvCreateImage(cvGetSize(rawImage), 8, 3);	
+	// 给yuvImage 分配一个和rawImage 尺寸相同,8位3通道图像
+	ImaskCodeBook = cvCreateImage(cvGetSize(rawImage), IPL_DEPTH_8U, 1);
+	// 为ImaskCodeBook 分配一个和rawImage 尺寸相同,8位单通道图像
+	
+	ImaskCodeBookCC = cvCreateImage(cvGetSize(rawImage), IPL_DEPTH_8U, 1);
+	
+	cvSet(ImaskCodeBook, cvScalar(255));
+	// 设置单通道数组所有元素为255,即初始化为白色图像
+	
+	imageLen = rawImage->width * rawImage->height;
+	cB = new codeBook[imageLen];
+	// 得到与图像像素数目长度一样的一组码本,以便对每个像素进行处理
+	
+	for (int i=0; i<imageLen; i++)
+		// 初始化每个码元数目为0
+		cB[i].numEntries = 0;
+	for (int i=0; i<nChannels; i++)
+	{
+		cbBounds[i] = 10;	// 用于确定码元各通道的阀值
+
+		minMod[i]	= 20;	// 用于背景差分函数中
+		maxMod[i]	= 20;	// 调整其值以达到最好的分割
+	}
+		
+	
+	//////////////////////////////////////////////////////////////////////////
+	// 开始处理视频每一帧图像
+	for (int i=0;;i++)
+	{
+		cvCvtColor(rawImage, yuvImage, CV_BGR2YCrCb);
+		// 色彩空间转换,将rawImage 转换到YUV色彩空间,输出到yuvImage
+		// 即使不转换效果依然很好
+		// yuvImage = cvCloneImage(rawImage);
+
+		if (i <= 30)
+			// 30帧内进行背景学习
+		{
+			pColor = (uchar *)(yuvImage->imageData);
+			// 指向yuvImage 图像的通道数据
+			for (int c=0; c<imageLen; c++)
+			{
+				cvupdateCodeBook(pColor, cB[c], cbBounds, nChannels);
+				// 对每个像素,调用此函数,捕捉背景中相关变化图像
+				pColor += 3;
+				// 3 通道图像, 指向下一个像素通道数据
+			}
+			if (i == 30)
+				// 到30 帧时调用下面函数,删除码本中陈旧的码元
+			{
+				for (int c=0; c<imageLen; c++)
+					cvclearStaleEntries(cB[c]);
+			}
+		}
+		else
+		{
+			uchar maskPixelCodeBook;
+			pColor = (uchar *)((yuvImage)->imageData); //3 channel yuv image
+			uchar *pMask = (uchar *)((ImaskCodeBook)->imageData); //1 channel image
+			// 指向ImaskCodeBook 通道数据序列的首元素
+			for(int c=0; c<imageLen; c++)
+			{
+				maskPixelCodeBook = cvbackgroundDiff(pColor, cB[c], nChannels, minMod, maxMod);
+				// 我看到这儿时豁然开朗,开始理解了codeBook 呵呵
+				*pMask++ = maskPixelCodeBook;
+				pColor += 3;
+				// pColor 指向的是3通道图像
+			}
+			cvCopy(ImaskCodeBook, ImaskCodeBookCC);	
+			cvconnectedComponents(ImaskCodeBookCC, 1, 4.0);
+		}
+		if (!(rawImage = cvQueryFrame(capture)))
+			break;
+		cvShowImage("Raw", rawImage);
+		cvShowImage("CodeBook", ImaskCodeBook);
+		cvShowImage("CodeBook_ConnectComp", ImaskCodeBookCC);
+
+		if (cvWaitKey(30) == 27)
+			break;
+		if (i == 56 || i == 63)
+			cvWaitKey();
+	}	
+	
+	cvReleaseCapture(&capture);
+	if (yuvImage)
+		cvReleaseImage(&yuvImage);
+	if(ImaskCodeBook) 
+		cvReleaseImage(&ImaskCodeBook);
+	cvDestroyAllWindows();
+	delete [] cB;
+
+	return 0;
+}
---
+++ opencv/平均背景减除.cpp
@@ -0,0 +1,239 @@
+﻿
+/***
+平均背景法进行图像分割, 在图像背景相对稳定的情况下, 检测闯入图像的前景目标
+*/
+/***
+	Averaging Background Method
+	We’ve just seen a simple method of learning background scenes and segmenting fore-
+	ground objects. It will work well only with scenes that do not contain moving background 
+	components (like a waving curtain or waving trees). It also assumes that the lighting 
+	remains fairly constant (as in indoor static scenes). 
+*/
+#include "stdafx.h"
+#include "cv.h"
+#include "highgui.h"
+
+/***************************************************/
+//我们为需要的不同临时图像和统计属性的图像创建指针
+
+//Float 3-channel images
+IplImage *IavgF, *IdiffF, *IprevF, *IhiF, *IlowF;
+IplImage *Iscratch, *Iscratch2;
+
+//Float 1-channel images
+IplImage *Igray1, *Igray2, *Igray3;
+IplImage *Ilow1, *Ilow2, *Ilow3;
+IplImage *Ihi1, *Ihi2, *Ihi3;
+
+//Byte 1-channel image
+IplImage *Imaskt;
+IplImage *Imask;
+
+float Icount;
+/**************************************************/
+
+
+void AllocateImages(IplImage* I)
+//该函数为需要的所有临时图像分配内存,传入来自视频的首帧图像作为大小参考
+{
+	CvSize sz = cvGetSize(I);
+
+	IavgF	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+	IdiffF	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+	IprevF	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+	IhiF	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+	IlowF	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+
+	Ilow1	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Ilow2	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Ilow3	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Ihi1	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Ihi2	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Ihi3	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+
+	cvZero(IavgF);
+	cvZero(IdiffF);
+	cvZero(IprevF);
+	cvZero(IhiF);
+	cvZero(IlowF);
+	
+	Icount = 1e-5;
+
+	Iscratch	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+	Iscratch2	= cvCreateImage(sz, IPL_DEPTH_32F, 3);
+
+	cvZero(Iscratch);
+	cvZero(Iscratch2);
+
+	Igray1	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Igray2	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Igray3	= cvCreateImage(sz, IPL_DEPTH_32F, 1);
+	Imaskt	= cvCreateImage(sz, IPL_DEPTH_8U, 1);
+	Imask	= cvCreateImage(sz, IPL_DEPTH_8U, 1);
+
+}
+
+void accumulateBackground(IplImage* I)
+//累积背景图像和前后帧图像差值的绝对值
+//当累积够一定数量后就将其转换成一个背景统计模型
+{
+	static int first = 1;
+	//局部静态变量,只初始化一次,意思就是第一次被赋值为1
+
+	cvCvtScale(I, Iscratch, 1, 0);
+	//将I指向的图像复制给Iscratch 不能用cvCopy,因为像素的位深度不同
+
+	if (!first)
+	{
+		cvAcc(Iscratch, IavgF);
+		//累积原始的浮点图像到IIavgF
+		cvAbsDiff(Iscratch, IprevF, Iscratch2);
+		//计算前后帧图像绝对差图像到Iscratch2
+		cvAcc(Iscratch2, IdiffF);
+		//将前后帧差值图像累加到IdiffF 中
+		Icount += 1.0;
+		//记录累加的次数用于背景统计时计算均值
+	}
+	first = 0;
+	//first 为局部静态变量,以后调用该函数将不再初始化为1
+	//意思就是除了第一次,以后调用该函数均进入if 语句
+
+	cvCopy(Iscratch, IprevF);
+	//IprevF用来保存前一帧图像
+}
+
+void setHighThreshold(float scale)
+{
+	cvConvertScale(IdiffF, Iscratch, scale);
+	//将统计的绝对差分图像值放大scale 倍赋给Iscratch
+	cvAdd(Iscratch, IavgF, IhiF);
+	//IhiF = Iscratch + IavgF 
+	cvSplit(IhiF, Ihi1, Ihi2, Ihi3, 0);
+	//将阀值上限分割为多通道
+}
+
+void setLowThreshold(float scale)
+{
+	cvConvertScale(IdiffF, Iscratch, scale);
+	cvSub(IavgF, Iscratch, IlowF);
+	//IlowF = IavgF - Iscratch
+	cvSplit(IlowF, Ilow1, Ilow2, Ilow3, 0);
+	//将阀值下限分割为多通道
+}
+
+void createModelsfromStats()
+//当累积足够多的帧图后,就将其转化成一个背景统计模型
+//该函数用于计算每个像素的均值和平均绝对差分
+{
+	cvConvertScale(IavgF, IavgF, (double)(1.0/Icount));
+	//计算平均原始图像到 IavgF
+	cvConvertScale(IdiffF, IdiffF, (double)(1.0/Icount));
+	//计算绝对差分图像到 IdiffF
+
+	cvAddS(IdiffF, cvScalar(1.0, 1.0, 1.0), IdiffF);
+	//使得到的绝对差分图像每个像素值均不为空
+
+	setHighThreshold(7.0);//使得对于每一帧图像的绝对差大于平均值7倍的像素都被认为是前景
+	setLowThreshold(6.0);//使得对于每一帧图像的绝对差小于平均值6倍的像素都被认为是前景
+	//根据统计的背景模型设定一个阀值上限和下限
+	//如果 IlowF <= Temp < IhiF 时认为其为背景,否则为视频中出现的运动目标物体
+}
+
+
+//图像分割
+void backgroundDiff(IplImage* I)
+{
+	cvCvtScale(I, Iscratch, 1, 0);
+	//转换成浮点型图像：将I指向的图像复制给Iscratch 不能用cvCopy, 因为像素的位深度不同
+	cvSplit(Iscratch, Igray1, Igray2, Igray3, 0);
+	//得到的当前帧分割成3个单通道图像
+	cvInRange(Igray1, Ilow1, Ihi1, Imask);
+	//        src     lower  upper dst
+	//检查这些单通道图像是否在平均背景像素高低阀值之间
+	//如果src(I)在范围内(lower <= src < upper)dst(I)被设置为0xff(每一位都是 '1')否则置0
+	cvInRange(Igray2, Ilow2, Ihi2, Imaskt);
+	cvOr(Imask, Imaskt, Imask);
+	//计算两个数组每个元素的按位或值赋值给第三个参数
+	cvInRange(Igray3, Ilow3, Ihi3, Imaskt);
+	cvOr(Imask, Imaskt, Imask);
+	//最后Imask 为分离出的前景二值图
+	cvSubRS(Imask, cvScalar(255), Imask);
+	//计算数量和数组之间的差,将Imask反相处理
+}
+
+void DeallocateImages()
+//解除分配的内存
+{
+	cvReleaseImage(&IavgF);
+	cvReleaseImage(&IdiffF);
+	cvReleaseImage(&IprevF);
+	cvReleaseImage(&IhiF);
+	cvReleaseImage(&IlowF);
+	cvReleaseImage(&Ilow1);
+	cvReleaseImage(&Ilow2);
+	cvReleaseImage(&Ilow3);
+	cvReleaseImage(&Ihi1);
+	cvReleaseImage(&Ihi2);
+	cvReleaseImage(&Ihi3);
+	cvReleaseImage(&Iscratch);
+	cvReleaseImage(&Iscratch2);
+	cvReleaseImage(&Igray1);
+	cvReleaseImage(&Igray2);
+	cvReleaseImage(&Igray3);
+	cvReleaseImage(&Imaskt);
+	cvReleaseImage(&Imask);
+}
+
+int main()
+{
+	CvCapture* capture = cvCreateFileCapture("D:\\vs程序\\vs2010\\第9章\\616.MP4");
+	//初始化从文件中获取视频
+	if (!capture)
+	{
+		printf("Couldn't Open the file.");
+		return -1;
+	}
+	
+	cvNamedWindow("raw");
+	cvNamedWindow("avg");
+
+	IplImage* rawImage = cvQueryFrame(capture);
+	//这个函数仅仅是函数cvGrabFrame和函数cvRetrieveFrame在一起调用的组合
+	cvShowImage("raw", rawImage);
+	
+	AllocateImages(rawImage);//调用子函数
+	
+	for (int i=0;;i++)
+	{
+		if (i <= 30) 
+		{
+			accumulateBackground(rawImage);
+			//前30帧用于累积计算背景图像
+			if (i == 30)
+				//将前30真转换成一个背景统计模型
+				createModelsfromStats();
+		}
+		else 
+			//建立好背景模型后调用此函数进行图像分割
+			backgroundDiff(rawImage);
+
+		cvShowImage("avg", Imask);
+		//播放分割后的目标图像结果
+
+		if (cvWaitKey(33) == 27)
+			//每33ms 播放一帧
+			break;
+
+		if (!(rawImage = cvQueryFrame(capture)))
+			break;
+		cvShowImage("raw", rawImage);
+		//显示原图像
+
+		if (i == 56 || i == 63)
+			//56帧和63帧时暂停
+			cvWaitKey();
+	}	
+
+	DeallocateImages();
+	return 0;
+}
